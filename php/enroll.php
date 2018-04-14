ce53b597acb96aab83582a6b92e1e0580d91f5a3
xuexiaojian <1069961428@qq.com>
2018-04-14 13:16:40 +0800

    这次交全了2

--- php/enroll.php
+++ php/enroll.php
@@ -5,7 +5,7 @@
 	$phonenum = $_POST["phonenum"];
 	$password = $_POST["password"];
 	
-	$con = mysql_connect("localhost", "root", "123456");
+	$con = mysql_connect("sqld.duapp.com", "star灬夼舞", "1069961428czj");
 	if($con){
 		echo "success";
 		
@@ -14,7 +14,7 @@
 		exit;
 	}
 	
-	mysql_select_db("zolenroll");
+	mysql_select_db("iNCTkSuSAGQKouIaQyeT");
 	$sql = "";
 	$sq = "SELECT name FROM enroll WHERE name = '$name';";
 	$res = mysql_query($sq);
